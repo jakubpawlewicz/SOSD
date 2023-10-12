6127e5541ac616ae606e41f01520167f8d6806ac
Jakub Pawlewicz <pan@mimuw.edu.pl>
2023-10-12 11:19:36 +0200

    Refactored running multiple paramaters

--- benchmarks/benchmark_fastmap.cc
+++ benchmarks/benchmark_fastmap.cc
@@ -4,31 +4,57 @@
 #include "benchmarks/common.h"
 #include "competitors/fastmap.h"
 
+namespace {
+
+template <class... Args>
+struct Applier
+{
+  template <class F>
+  static void apply(F f, Args... args)
+  {
+    f(args...);
+  }
+};
+
+template <class T, T... I, class... Args>
+struct Applier<std::integer_sequence<T, I...>, Args...>
+{
+  template <class F>
+  static void apply(F f, std::integer_sequence<T, I...>, Args... args)
+  {
+    (Applier<Args..., std::integral_constant<T, I>>::apply(f, args..., std::integral_constant<T, I>()), ...);
+  }
+};
+
+template <class F, class... Args>
+void apply(F f, Args... args)
+{
+  Applier<Args...>::apply(f, args...);
+}
+
+};
+
 template <template <class, int> class Index, int size_scale, class T, template <typename> typename Searcher>
 void benchmark_run(sosd::Benchmark<T, Searcher>& benchmark) {
   benchmark.template Run<Index<T, size_scale>>();
 }
 
-template <template <class, int> class Index, int... seq, class T, template <typename> typename Searcher>
-void benchmark_run_seq(sosd::Benchmark<T, Searcher>& benchmark, std::integer_sequence<int, seq...>) {
-  (benchmark_run<Index, seq>(benchmark), ...);
+namespace {
+template <template <class, int> class Index, class T, template <typename> typename Searcher,
+          class F, class... Args>
+void benchmark_run_help(sosd::Benchmark<T, Searcher>& benchmark, F f, Args... args) {
+  ::apply([=, &benchmark](auto... a) { benchmark.template Run<Index<T, f(a...)>>(); }, args...);
 }
 
-template <template <class, int> class Index, int val1, int... seq2, class F, class T, template <typename> typename Searcher>
-void benchmark_run_two_seqs(sosd::Benchmark<T, Searcher>& benchmark, F f, std::integer_sequence<int, seq2...>) {
-  (benchmark_run<Index, f(val1, seq2)>(benchmark), ...);
-}
+constexpr auto id = [](auto x) { return x; };
 
-template <template <class, int> class Index, int... seq1, int... seq2, class F, class T, template <typename> typename Searcher>
-void benchmark_run_two_seqs(sosd::Benchmark<T, Searcher>& benchmark, F f, std::integer_sequence<int, seq1...>, std::integer_sequence<int, seq2...> s2) {
-    (benchmark_run_two_seqs<Index, seq1>(benchmark, f, s2), ...);
-}
+};
 
 template <class T, template <typename> typename Searcher>
 void benchmark_fastmap_bucket(sosd::Benchmark<T, Searcher>& benchmark, bool pareto) {
-  benchmark_run_seq<FastMapBucket>(benchmark, std::integer_sequence<int, 1>{});
+  benchmark_run_help<FastMapBucket>(benchmark, id, std::integer_sequence<int, 1>{});
   if (pareto)
-    benchmark_run_seq<FastMapBucket>(benchmark, std::integer_sequence<int, 2, 4, 8, 16, 32, 64, 128, 256, 1024, 4096>{});
+    benchmark_run_help<FastMapBucket>(benchmark, id, std::integer_sequence<int, 2, 4, 8, 16, 32, 64, 128, 256, 1024, 4096>{});
 }
 
 template <template <typename> typename Searcher>
@@ -45,13 +71,13 @@ void benchmark_64_fastmap_bucket(sosd::Benchmark<uint64_t, Searcher>& benchmark,
 
 template <class T, template <typename> typename Searcher>
 void benchmark_fastmap_pgmfull(sosd::Benchmark<T, Searcher>& benchmark, bool pareto) {
-  auto f = [](auto x, auto y) { return 100 * x + y; };
+  constexpr auto f = [](auto x, auto y) constexpr { return 100 * x + y; };
   if (pareto)
-    benchmark_run_two_seqs<FastMapPGMFull>(benchmark, f,
+    benchmark_run_help<FastMapPGMFull>(benchmark, f,
       std::integer_sequence<int, 8, 16, 32, 64, 128, 256, 1024, 4096>{},
       std::integer_sequence<int, 6, 8, 10, 12, 16>{});
   else
-    benchmark_run_two_seqs<FastMapPGMFull>(benchmark, f,
+    benchmark_run_help<FastMapPGMFull>(benchmark, f,
       std::integer_sequence<int, 34>{},
       std::integer_sequence<int, 10>{});
 }
@@ -70,13 +96,13 @@ void benchmark_64_fastmap_pgmfull(sosd::Benchmark<uint64_t, Searcher>& benchmark
 
 template <class T, template <typename> typename Searcher>
 void benchmark_fastmap_pgmbucket(sosd::Benchmark<T, Searcher>& benchmark, bool pareto) {
-  auto f = [](auto x, auto y) { return 100 * x + y; };
+  constexpr auto f = [](auto x, auto y) { return 100 * x + y; };
   if (pareto)
-    benchmark_run_two_seqs<FastMapPGMBucket>(benchmark, f,
+    benchmark_run_help<FastMapPGMBucket>(benchmark, f,
       std::integer_sequence<int, 8, 16, 32, 64, 128, 256, 1024, 4096>{},
       std::integer_sequence<int, 1, 2, 4>{});
   else
-    benchmark_run_two_seqs<FastMapPGMBucket>(benchmark, f,
+    benchmark_run_help<FastMapPGMBucket>(benchmark, f,
       std::integer_sequence<int, 34>{},
       std::integer_sequence<int, 1>{});
 }
@@ -95,13 +121,13 @@ void benchmark_64_fastmap_pgmbucket(sosd::Benchmark<uint64_t, Searcher>& benchma
 
 template <class T, template <typename> typename Searcher>
 void benchmark_fastmap_apx(sosd::Benchmark<T, Searcher>& benchmark, bool pareto) {
-  auto f = [](auto x, auto y) { return 1000 * x + y; };
+  constexpr auto f = [](auto x, auto y) { return 1000 * x + y; };
   if (pareto)
-    benchmark_run_two_seqs<FastMapApx>(benchmark, f,
+    benchmark_run_help<FastMapApx>(benchmark, f,
       std::integer_sequence<int, 8, 16, 32, 64, 128, 256>{},
       std::integer_sequence<int, 15, 20, 40, 60, 80, 120>{});
   else
-    benchmark_run_two_seqs<FastMapApx>(benchmark, f,
+    benchmark_run_help<FastMapApx>(benchmark, f,
       std::integer_sequence<int, 16>{},
       std::integer_sequence<int, 100>{});
 }
